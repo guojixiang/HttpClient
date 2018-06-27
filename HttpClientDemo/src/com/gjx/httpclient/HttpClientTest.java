113ad0a30066c49facb8a1712cf7dcf6d8a562b1
600033 <guojixiang@syswin.com>
2018-06-28 07:23:45 +0800

    add httpClient-post method

--- HttpClientDemo/src/com/gjx/httpclient/HttpClientTest.java
+++ HttpClientDemo/src/com/gjx/httpclient/HttpClientTest.java
@@ -3,17 +3,23 @@ package com.gjx.httpclient;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.alibaba.fastjson.JSON;
+
 public class HttpClientTest {
 
 	public static void main(String[] args) {
 		HttpClientUtil httpClientUtil = new HttpClientUtil();
-		String url = "https://www.baidu.com";
+//		String url = "http://localhost:8081/postTest";
+		String url = "https://useroauth.systoon.com/oauth/authorize";
+//		Map<String,String> params = new HashMap<>();
 		Map<String,String> params = new HashMap<>();
-//		params.put("pageNum", "1");
-//		params.put("pageSize", "10");
-		Map<String,String> headers = null;
+		params.put("response_type", "code");
+		Map<String,String> headers = new HashMap<>();
+//		headers.put("Content-Type", "application/json;");
 		Class clazz = Result.class;
 		Result result = (Result) httpClientUtil.syncGetHttp(url, params, headers, clazz);
+//		Result result = (Result) httpClientUtil.syncPostHttp(url, params, null, headers, clazz);
+		System.out.println("ÇëÇó½á¹û£º\n"+ JSON.toJSONString(result));
 	}
 
 }
--- HttpClientDemo/src/com/gjx/httpclient/HttpClientUtil.java
+++ HttpClientDemo/src/com/gjx/httpclient/HttpClientUtil.java
@@ -1,60 +1,107 @@
 package com.gjx.httpclient;
 
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import com.alibaba.fastjson.JSON;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
  * Created by 600033 on 2018/6/26.
  */
 public class HttpClientUtil {
+	
+	private static final String CHARSET = "UTF-8";
 	static HttpClientPoolConfig httpClientPoolConfig;
 	static {
 		httpClientPoolConfig = new HttpClientPoolConfig();
 	}
 
-    public <T> T syncGetHttp(String url,Map<String,String> params,Map<String,String> headers, Class<T> clazz) {
-        try {
-            StringBuilder urlPairs = new StringBuilder("?");
-            CloseableHttpClient closeableHttpClient = httpClientPoolConfig.getHttpClient();
-            if(!url.startsWith("http://") && !url.startsWith("https://")) {
-                url = "http://"+url;
-            }
+	public <T> T syncGetHttp(String url, Map<String, String> params, Map<String, String> headers, Class<T> clazz) {
+		try {
 
-            if(params != null && params.size()>0) {
-                for (Map.Entry entry: params.entrySet()) {
-                    urlPairs.append(entry.getKey()).append("=").append(entry.getValue()).append("&");
-                }
-                if(urlPairs.toString().length()>1) {
-                    urlPairs.deleteCharAt(urlPairs.toString().length()-1);
-                }
-            }
-            url += urlPairs.toString();
-            HttpGet httpGet = new HttpGet(url);
-            if(headers != null) {
-                headers.forEach((K,V)->{
-                    httpGet.setHeader(K,V);
-                });
-            }
-            CloseableHttpResponse response = closeableHttpClient.execute(httpGet);
-            HttpEntity httpEntity = response.getEntity();
-            String resultStr = EntityUtils.toString(httpEntity,"utf-8");
-            if(response.getStatusLine().getStatusCode() <200 || response.getStatusLine().getStatusCode()>300) {
-                throw new Exception("接口请求失败，状态码:"+response.getStatusLine().getStatusCode()+"\n"+resultStr);
-            }
-            EntityUtils.consume(httpEntity);
-            System.out.println("请求结果：\n"+resultStr);
-            return JSON.parseObject(resultStr,clazz);
-//            return null;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+			CloseableHttpClient closeableHttpClient = httpClientPoolConfig.getHttpClient();
+			url = buildUriParameter(url, params);
+			HttpGet httpGet = new HttpGet(url);
+			if (headers != null) {
+				headers.forEach((K, V) -> {
+					httpGet.setHeader(K, V);
+				});
+			}
+			CloseableHttpResponse response = closeableHttpClient.execute(httpGet);
+			HttpEntity httpEntity = response.getEntity();
+			String resultStr = EntityUtils.toString(httpEntity, CHARSET);
+			if (response.getStatusLine().getStatusCode() < 200 || response.getStatusLine().getStatusCode() > 300) {
+				throw new Exception("接口请求失败，状态码:" + response.getStatusLine().getStatusCode() + "\n" + resultStr);
+			}
+			EntityUtils.consume(httpEntity);
+			System.out.println("请求结果：\n" + resultStr);
+			return JSON.parseObject(resultStr, clazz);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public <T> T syncPostHttp(String url,Map<String,String> uriParams,Map<String,Object> body,Map<String,String> headers ,Class<T> clazz) {
+		try {
+			CloseableHttpClient closeableHttpClient = httpClientPoolConfig.getHttpClient();
+			HttpPost httpPost = new HttpPost(url);
+			if(body != null) {
+				StringEntity stringEntity = new StringEntity(JSON.toJSONString(body));
+				httpPost.setEntity(stringEntity);
+			} else if(uriParams != null && uriParams.size()>0) {
+				List<NameValuePair> paramArray = new ArrayList<>();
+				for (Map.Entry<String, String> entry : uriParams.entrySet()) {
+					paramArray.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+				}
+				httpPost.setEntity(new UrlEncodedFormEntity(paramArray,CHARSET));
+			}
+			if(headers != null && headers.size() > 0) {
+				for(Map.Entry<String, String> entry: headers.entrySet()) {
+					httpPost.setHeader(entry.getKey(), entry.getValue());
+				}
+			}
+			CloseableHttpResponse httpResponse = closeableHttpClient.execute(httpPost);
+			HttpEntity httpEntity = httpResponse.getEntity();
+			String resultStr = EntityUtils.toString(httpEntity,CHARSET);
+			if(httpResponse.getStatusLine().getStatusCode() < 200 || httpResponse.getStatusLine().getStatusCode()>300) {
+				throw new Exception("接口请求失败，状态码:" + httpResponse.getStatusLine().getStatusCode() + "\n" + resultStr);
+			}
+			return JSON.parseObject(resultStr,clazz);
+		}catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private String buildUriParameter(String url, Map<String, String> params) {
+		StringBuilder urlPairs = new StringBuilder("?");
+		if (!url.startsWith("http://") && !url.startsWith("https://")) {
+			url = "http://" + url;
+		}
+		if (params != null && params.size() > 0) {
+			for (Map.Entry entry : params.entrySet()) {
+				urlPairs.append(entry.getKey()).append("=").append(entry.getValue()).append("&");
+			}
+			if (urlPairs.toString().length() > 1) {
+				urlPairs.deleteCharAt(urlPairs.toString().length() - 1);
+			}
+		}
+		url += urlPairs.toString();
+		return url;
+	}
+
 }
